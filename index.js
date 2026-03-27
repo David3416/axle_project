8482a6b9b75a526ea23324d5422a5468fbb06219
David3415 <dgagiev@gmail.com>
2026-03-27 14:56:53 +0300

    folders added

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-const http = require('http')
-const fs = require('fs')
-//const db = require('./db')
-
 const express = require("express");
+const path = require("path");
+
 const app = express();
+
 app.use(express.static("public"));
 
-let server = http.createServer((req, res) => {
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "templates", "index.html"));
+});
 
- res.writeHead(200, { 'Content-Type': 'text/html' })
- 
- const stream = fs.createReadStream('./templates/index.html')
- stream.pipe(res)
-})
-
-const PORT = 3000
-const HOST = 'localhost'
-
-server.listen(PORT,HOST, () => {
-  console.log('Server running on http://localhost:3000')
-})
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
